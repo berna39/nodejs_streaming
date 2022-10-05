afa0ceaa5f042a7b581d8fd1eafe218e4881698d
berna39 <kalemajoseph39@gmail.com>
2022-10-05 23:40:44 +0200

    Backpreasure problem

--- backpreasure.js
+++ backpreasure.js
@@ -1,9 +1,14 @@
 const  { createWriteStream, createReadStream } = require('fs');
 
 let readStream = createReadStream('./files/naruto.mp4'); // origin
-let writeStream = createWriteStream(`./files/copy${new Date().getMilliseconds}.mp4`); // destination
+let writeStream = createWriteStream(`./files/copy${new Date().getTime()}.mp4`); // destination
 
-readStream.on("data", (chunk) => { writeStream.write(chunk) }); // reading by writing
+readStream.on("data", (chunk) => { 
+    let result = writeStream.write(chunk); // this is gonna use a lot of memory -> because it's returning false, what means that the writable stream cannot get any more data
+    console.log(result);
+}); // reading by writing
+
+
 readStream.on("error", (err) => { console.log(`Error occured : ${err}`) });
 readStream.on("end", () => writeStream.end());
 writeStream.on("close", () => { console.log(`The copy file is ready`) });
