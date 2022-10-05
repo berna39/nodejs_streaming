b7cfddd731afe6c83b1a25fa836797a48cde8558
berna39 <kalemajoseph39@gmail.com>
2022-10-05 15:03:32 +0200

    Non flowing mode of streams

--- MyReadableFs.js
+++ MyReadableFs.js
@@ -4,3 +4,10 @@ let myStream = ReadStream('./files/naruto.mp4');
 
 myStream.on("data", (data) => { console.log(data); });
 myStream.on("data", (data) => { console.log(`size of the chunk : ${data.length}`); });
+myStream.on("error", (err) => console.log(`Error occured : ${err}`));
+myStream.on("end", () => { console.log(`No more data on the stream`) });
+
+//--- To put the stream into the non-flowing mode ---//
+
+// myStream.pause(); // To stop the reading
+// myStream.read(); // To continue the reading
