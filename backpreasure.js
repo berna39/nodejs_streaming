6e58c74e9e120cc5c447e4516972bcfa9b1d5195
berna39 <kalemajoseph39@gmail.com>
2022-10-05 23:54:57 +0200

    highWaterMark concept

--- backpreasure.js
+++ backpreasure.js
@@ -1,7 +1,9 @@
 const  { createWriteStream, createReadStream } = require('fs');
 
 let readStream = createReadStream('./files/naruto.mp4'); // origin
-let writeStream = createWriteStream(`./files/copy${new Date().getTime()}.mp4`); // destination
+let writeStream = createWriteStream(`./files/copy${new Date().getTime()}.mp4`, {
+    highWaterMark: 955360 // at the moment that we increase the highWaterMark, there won't be a lot of backpresure but we'll be using a lot of memory
+}); // destination
 
 readStream.on("data", (chunk) => { 
     let result = writeStream.write(chunk); // this is gonna use a lot of memory -> because it's returning false, what means that the writable stream cannot get any more data
@@ -20,3 +22,5 @@ writeStream.on("drain", (err) => { // in case we're now ready to read more data
 });
 readStream.on("end", () => writeStream.end());
 writeStream.on("close", () => { console.log(`The copy file is ready`) });
+
+// In this case we're not going to use a lot of memory
