44d9675b7b27f9b1fffd58cf05bf505d397c57b2
berna39 <kalemajoseph39@gmail.com>
2022-10-06 22:24:50 +0200

    Piping stdin stream to txt file

--- Pipe.js
+++ Pipe.js
@@ -1,7 +1,13 @@
 const  { createWriteStream, createReadStream } = require('fs');
 
-let readStream = createReadStream('./files/naruto.mp4'); // origin
-let writeStream = createWriteStream(`./files/copy${new Date().getTime()}.mp4`,); // destination
+// let readStream = createReadStream('./files/naruto.mp4'); // origin
+// let writeStream = createWriteStream(`./files/copy${new Date().getTime()}.mp4`); // destination
 
-// The pipe method does the exact same things with our home made listeners and handles backpreasure for us
-readStream.pipe(writeStream).on("error", (err) => console.log(`Error occured`));
+// // The pipe method does the exact same things with our home made listeners and handles backpreasure for us
+// readStream.pipe(writeStream).on("error", (err) => console.log(`Error occured`));
+
+
+// ----- Second example -------- //
+let writeStream = createWriteStream(`./files/myFile${new Date().getTime()}.txt`);
+// piping the incoming stream to myFile... -> cmg  echo "Hello berna" | node Pipe.js 
+process.stdin.pipe(writeStream);
